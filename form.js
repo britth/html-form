1cee9dfcb5ad123594829e0cce470d206309edfa
britth <brittmhayes@gmail.com>
2017-05-02 11:46:35 -0400

    Fix typo, refactor form.js

--- form.js
+++ form.js
@@ -13,71 +13,59 @@ $(function() {
     var selectedFrogs = selectedValue === "Frogs"
 
     if (validText && isChecked && selectedFrogs && validDropdown) {
-      console.log('VALID');
-    } else if (validText && isChecked && selectedFrogs) {
-      console.log('ERROR');
-      alert('Plese make a selection in the dropdown')
-    } else if (validText && isChecked && validDropdown) {
-      console.log('ERROR');
-      alert('Please select the value "Frogs" (You selected "'
-        + selectedValue+'")')
-    } else if (validText && selectedFrogs && validDropdown) {
-      console.log('ERROR');
-      alert('Please check "Have you entered everything"')
-    } else if (selectedFrogs && validDropdown && isChecked){
-      console.log('ERROR');
-      alert('Please enter "5" in the text box (You entered "'
-        + actualValue + '")')
-    } else if (validText && isChecked) {
-      console.log('ERROR');
-      alert('Please select the value "Frogs" (You selected "'
-        + selectedValue+'"). Select a value in the dropdown')
-    } else if (validText && selectedFrogs) {
-      console.log('ERROR')
-      alert('Please select a value in the dropdown. '
-        + 'Check "Have you entered everything"')
-    } else if (isChecked && selectedFrogs) {
-      console.log('ERROR')
-      alert('Please enter "5" in the text box (You entered "'
-        + actualValue + '"). Select a value in the dropdown')
-    } else if (validText && validDropdown){
-      console.log('ERROR')
-      alert('Please select the value "Frogs" (You selected "'
-        + selectedValue+'"). Check "Have you entered everything"')
-    } else if (selectedFrogs && validDropdown){
-      console.log('ERROR')
-      alert('Please enter "5" in the text box (You entered "'
-        + actualValue + '"). Check "Have you entered everything"')
-    } else if (isChecked && validDropdown){
-      console.log('ERROR')
-      alert('Please enter "5" in the text box (You entered "'
-        + actualValue + '"). Select the value "Frogs" (You selected "'
-          + selectedValue+'"). Select a value in the dropdown')
-    } else if (validText){
-      console.log('ERROR')
-      alert('Please select the value "Frogs" (You selected "'
-        + selectedValue + '"). Select a value in the dropdown. '
-        + 'Check "Have you entered everything"')
-    } else if (isChecked){
-      console.log('ERROR')
-      alert('Please enter "5" in the text box (You entered "'
-        + actualValue + '"). Select the value "Frogs" (You selected "'
-        + selectedValue + '"). Select a value in the dropdown')
-    } else if (selectedFrogs){
-      console.log('ERROR')
-      alert('Please enter "5" in the text box (You entered "'
-        + actualValue + '"). Select a value in the dropdown. '
-        + 'Check "Have you entered everything"')
-    } else if (validDropdown) {
-      console.log('ERROR')
-      alert('Please enter "5" in the text box (You entered "'
-        + actualValue + '"). Select the value "Frogs" (You selected '
-          + selectedValue + '). Check "Have you entered everything"')
+        console.log('VALID');
     } else {
-      console.log("ERROR");
-      alert('There was a problem with your request. Type "5" in the text box,'
-        + ' select "Frogs", select a value in the dropdown, '
-        + 'and check "Have you entered everything"');
+      console.log('ERROR');
+      if (validText && isChecked && selectedFrogs) {
+        alert('Plese make a selection in the dropdown')
+      } else if (validText && isChecked && validDropdown) {
+        alert('Please select the value "Frogs" (You selected "'
+          + selectedValue+'")')
+      } else if (validText && selectedFrogs && validDropdown) {
+        alert('Please check "Have you entered everything?"')
+      } else if (selectedFrogs && validDropdown && isChecked){
+        alert('Please enter "5" in the text box (You entered "'
+          + actualValue + '")')
+      } else if (validText && isChecked) {
+        alert('Please select the value "Frogs" (You selected "'
+          + selectedValue+'"). Select a value in the dropdown')
+      } else if (validText && selectedFrogs) {
+        alert('Please select a value in the dropdown. '
+          + 'Check "Have you entered everything?"')
+      } else if (isChecked && selectedFrogs) {
+        alert('Please enter "5" in the text box (You entered "'
+          + actualValue + '"). Select a value in the dropdown')
+      } else if (validText && validDropdown){
+        alert('Please select the value "Frogs" (You selected "'
+          + selectedValue+'"). Check "Have you entered everything?"')
+      } else if (selectedFrogs && validDropdown){
+        alert('Please enter "5" in the text box (You entered "'
+          + actualValue + '"). Check "Have you entered everything?"')
+      } else if (isChecked && validDropdown){
+        alert('Please enter "5" in the text box (You entered "'
+          + actualValue + '"). Select the value "Frogs" (You selected "'
+            + selectedValue+'"). Select a value in the dropdown')
+      } else if (validText){
+        alert('Please select the value "Frogs" (You selected "'
+          + selectedValue + '"). Select a value in the dropdown. '
+          + 'Check "Have you entered everything?"')
+      } else if (isChecked){
+        alert('Please enter "5" in the text box (You entered "'
+          + actualValue + '"). Select the value "Frogs" (You selected "'
+          + selectedValue + '"). Select a value in the dropdown')
+      } else if (selectedFrogs){
+        alert('Please enter "5" in the text box (You entered "'
+          + actualValue + '"). Select a value in the dropdown. '
+          + 'Check "Have you entered everything?"')
+      } else if (validDropdown) {
+        alert('Please enter "5" in the text box (You entered "'
+          + actualValue + '"). Select the value "Frogs" (You selected '
+            + selectedValue + '). Check "Have you entered everything?"')
+      } else {
+        alert('There was a problem with your request. Type "5" in the text box,'
+          + ' select "Frogs", select a value in the dropdown, '
+          + 'and check "Have you entered everything?"');
+      }
     }
   };
 
